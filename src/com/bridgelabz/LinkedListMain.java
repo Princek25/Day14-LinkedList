cea222c2156f34d3f9e2255adf6dfc1fafff8517
Prince <princek2530@gmail.com>
2022-12-14 10:52:52 +0530

    Added: Ability to insert 30 between 56 and 70

--- src/com/bridgelabz/LinkedListMain.java
+++ src/com/bridgelabz/LinkedListMain.java
@@ -3,13 +3,17 @@ package com.bridgelabz;
 public class LinkedListMain {
     public static void main(String[] args) {
         MyNode<Integer> node1 = new MyNode<Integer>(56);
-        MyNode<Integer> node2 = new MyNode<Integer>(30);
+        MyNode<Integer> node2 = new MyNode<Integer>(70);
         MyNode<Integer> node3 = new MyNode<Integer>(76);
+        MyNode<Integer> node4 = new MyNode<Integer>(30);
 
         MyLinkedList list = new MyLinkedList();
         list.append(node1);
         list.append(node2);
         list.append(node3);
+        list.printMyNode();
+        System.out.println("Insert 30 in Between 56 and 70 ");
+        list.insert(node1, node4);
         list.printMyNode();
     }
 }
